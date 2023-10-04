68e858ffcd76c5a9fc5dc72445f211f13895267d
6545 FAIZAN AHMED <fazitanveer2@gmail.com>
2023-10-04 10:40:23 +0500

    Basic Task API Viewsets Created

--- To-Do/TaskAPI/models.py
+++ To-Do/TaskAPI/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
 # Create your models here.
+
+class Task(models.Model):
+  title = models.CharField(max_length=250)
+  description = models.TextField(null=True, blank=True)
+  completed = models.BooleanField(default=False, blank=True, null=True)
+      
+  def __str__(self) -> str:
+    return self.title
+  
+  class Meta:
+    ordering = ['title']
+  
--- To-Do/TaskAPI/views.py
+++ To-Do/TaskAPI/views.py
@@ -1,3 +1,46 @@
-from django.shortcuts import render
+from rest_framework.viewsets import ModelViewSet
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from .serializers import TaskSerializer
 
-# Create your views here.
+from .models import Task
+
+class TaskViewSet(ModelViewSet):
+    queryset = Task.objects.all()
+    serializer_class = TaskSerializer
+    
+    
+
+@api_view(['GET'])
+def apiOverview(request):
+    api_urls = {
+        'List': '/task-list/',
+        'Detail View': '/task-detail/<str:pk>/',
+        'Create': '/task-create/',
+        'Update': '/task-update/<str:pk>/',
+        'Delete': '/task-delete/<str:pk>/',
+    }
+
+    return Response(api_urls)
+
+	
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
